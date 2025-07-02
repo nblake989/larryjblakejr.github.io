dac0f67c130edbb032f0325b20b0464093a058d5
Nick Blake <8633545+nblake989@users.noreply.github.com>
2025-07-01 22:01:28 -0700

    updated broken JS for copyright year

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,3 @@
-    document.getElementById("year").innerHTML = new Date().getFullYear();
+// Find the copyright paragraph and replace "2023" with current year
+const copyrightText = document.querySelector('.copyright').innerHTML;
+document.querySelector('.copyright').innerHTML = copyrightText.replace('CURRENT_YEAR', new Date().getFullYear());
